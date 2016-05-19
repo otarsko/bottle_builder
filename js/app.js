fd9cfb8f693005426746476316006f3b8987d3e1
Oleksandr Tarasenko <oleksandr.tarasenko@mensemedia.net>
2016-05-19 13:49:59 +0300

    Add canvas on top of product image. Add Images panel.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
 
 angular.module('app', [])
     .controller('MainController', ['$scope', function($scope) {
-        $scope.canvas = new fabric.Canvas('bottle_canvas');
-        $scope.canvas.setHeight(800);
-        $scope.canvas.setWidth(500);
 
-        fabric.Image.fromURL('img/bottle.png', function(oImg) {
-            oImg.set('selectable', false);
-            $scope.canvas.add(oImg);
-        });
+        function initProducts() {
+            $scope.product = {
+                "name": "bottle",
+                "url": "img/bottle.png",
+                editableAreas: [
+                    {
+                        "name": "labelPanel",
+                        top: 260,
+                        left: 5,
+                        canvas: {height: 200, width: 115}
+                    }
+                ]
+            };
+        }
+
+        function initCanvas() {
+            $scope.canvas = new fabric.Canvas('bottle_canvas');
+            $scope.canvas.setHeight($scope.product.editableAreas[0].canvas.height);
+            $scope.canvas.setWidth($scope.product.editableAreas[0].canvas.width);
+
+            var elem = angular.element(document.querySelector(".canvas-container"));
+            elem.ready(function(){
+                elem.css('top', $scope.product.editableAreas[0].top + "px");
+                elem.css('left', $scope.product.editableAreas[0].left + "px");
+            });
+        }
+
+        //fabric.Image.fromURL('img/bottle.png', function(oImg) {
+        //    oImg.set('selectable', false);
+        //    $scope.canvas.add(oImg);
+        //});
+
+        function initIcons() {
+            $scope.images = ["img/logo/ic1.png", "img/logo/ic2.png", "img/logo/ic3.png",
+                "img/logo/ic4.png", "img/logo/ic5.png"];
+        }
+
+        initProducts();
+        initCanvas();
+        initIcons();
 
         $scope.canvas.renderAll();
     }]);
