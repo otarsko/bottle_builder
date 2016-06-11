1f2c19410bc1bb435fba9462a6f6f02184ab1f1a
AAlukard <Presario1>
2016-06-11 18:23:11 +0300

    Add tests for main service.

--- client/app/main/main.service.spec.js
+++ client/app/main/main.service.spec.js
@@ -5,14 +5,42 @@ describe('Service: mainService', function () {
   // load the service's module
   beforeEach(module('bottleBuilderApp'));
 
-  // instantiate service
   var mainService;
-  beforeEach(inject(function (_mainService_) {
+  var $httpBackend;
+
+  beforeEach(inject(function (_$httpBackend_, _mainService_) {
     mainService = _mainService_;
+    $httpBackend = _$httpBackend_;
+
+    $httpBackend.whenGET('/api/bottleDesigns/some_id')
+      .respond({canvas : 'some_canvas'});
+    $httpBackend.whenPOST('/api/bottleDesigns')
+      .respond({_id : 'some_id'});
   }));
 
-  //it('should do something', function () {
-  //  !!mainService.should.be.true;
-  //});
+  describe('loadBottleDesign', function() {
+    it('should load requested design and run callback on response', function () {
 
+      var canvasValue;
+      mainService.loadBottleDesign('some_id', function(response) {
+        canvasValue = response.data.canvas;
+      });
+      $httpBackend.flush();
+
+      canvasValue.should.equal('some_canvas');
+    });
+  });
+
+  describe('saveBottleDesign', function() {
+    it('should save canvas and run callback on response', function () {
+
+      var idFromResponse;
+      mainService.saveBottleDesign({toJSON: function() {return 'jsonValue'}}, function(response) {
+        idFromResponse = response.data._id;
+      });
+      $httpBackend.flush();
+
+      idFromResponse.should.equal('some_id')
+    });
+  });
 });
